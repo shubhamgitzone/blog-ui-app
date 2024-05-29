9edba83b358cf2531270eb5f75c8fb0c61e82302
shubham kumar <shubhamkmuz@gmail.com>
2024-05-29 06:54:22 +0530

    adding base component

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import Base from "../components/Base";
+
+const About=()=>{
+    return(
+       <Base>
+        <h1>This is about page</h1>
+        <p>We are building blog website</p>
+       </Base>
+    )
+};
+
+export default About;
